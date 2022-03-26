a6f8d46d499ed0b62e08c706b90ae14cf5d117ec
Julia Kosobokova <jilika12@yandex.ru>
2022-03-26 23:22:24 +0300

    feat: добавить поля ввода в форму

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,14 @@ const closeButton = popupElement.querySelector('body .popup__close-button');
 
 function openPopup() {
     popupElement.classList.add('popup_opened');
+
+    const nameInput = document.querySelector('.popup__input_type_name');
+    const descriptionInput = document.querySelector('.popup__input_type_description');
+    const nameElement = document.querySelector('.profile__name');
+    const descriptionElement = document.querySelector('.profile__description');
+
+    nameInput.value = nameElement.innerText;
+    descriptionInput.value = descriptionElement.innerText;
 }
 
 function closePopup() {
@@ -12,3 +20,25 @@ function closePopup() {
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
+const formElement = document.querySelector('.popup__form');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    const nameInput = document.querySelector('.popup__input_type_name');
+    const descriptionInput = document.querySelector('.popup__input_type_description');
+    const nameElement = document.querySelector('.profile__name');
+    const descriptionElement = document.querySelector('.profile__description');
+
+    nameElement.innerText = nameInput.value;
+    descriptionElement.innerText = descriptionInput.value;
+
+    popupElement.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
