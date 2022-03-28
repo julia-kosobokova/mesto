bce7c4f748fe24530aadea392940ed7ba5af0a83
Julia Kosobokova <jilika12@yandex.ru>
2022-03-28 22:19:18 +0300

    fix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
 const popupElement = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = popupElement.querySelector('body .popup__close-button');
+const closeButton = popupElement.querySelector('.popup__close-button');
+const nameInput = document.querySelector('.popup__input_type_name');
+const descriptionInput = document.querySelector('.popup__input_type_description');
+const nameElement = document.querySelector('.profile__name');
+const descriptionElement = document.querySelector('.profile__description');
+const formElement = document.querySelector('.popup__form');
 
 function openPopup() {
     popupElement.classList.add('popup_opened');
 
-    const nameInput = document.querySelector('.popup__input_type_name');
-    const descriptionInput = document.querySelector('.popup__input_type_description');
-    const nameElement = document.querySelector('.profile__name');
-    const descriptionElement = document.querySelector('.profile__description');
-
-    nameInput.value = nameElement.innerText;
-    descriptionInput.value = descriptionElement.innerText;
+    nameInput.value = nameElement.textContent;
+    descriptionInput.value = descriptionElement.textContent;
 }
 
 function closePopup() {
     popupElement.classList.remove('popup_opened');
 }
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-const formElement = document.querySelector('.popup__form');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -30,15 +25,12 @@ function formSubmitHandler(evt) {
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
 
-    const nameInput = document.querySelector('.popup__input_type_name');
-    const descriptionInput = document.querySelector('.popup__input_type_description');
-    const nameElement = document.querySelector('.profile__name');
-    const descriptionElement = document.querySelector('.profile__description');
+    nameElement.textContent = nameInput.value;
+    descriptionElement.textContent = descriptionInput.value;
 
-    nameElement.innerText = nameInput.value;
-    descriptionElement.innerText = descriptionInput.value;
-
-    popupElement.classList.remove('popup_opened');
+    closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
