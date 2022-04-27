3d5dca48f869b5148547e7a6f37fe798c2cbf6e3
Julia Kosobokova <jilika12@yandex.ru>
2022-04-27 23:22:35 +0300

    feat: добавит инициализацию карточек

--- scripts/index.js
+++ scripts/index.js
@@ -8,29 +8,73 @@ const descriptionElement = document.querySelector('.profile__description');
 const formElement = document.querySelector('.popup__form');
 
 function openPopup() {
-    popupElement.classList.add('popup_opened');
+  popupElement.classList.add('popup_opened');
 
-    nameInput.value = nameElement.textContent;
-    descriptionInput.value = descriptionElement.textContent;
+  nameInput.value = nameElement.textContent;
+  descriptionInput.value = descriptionElement.textContent;
 }
 
 function closePopup() {
-    popupElement.classList.remove('popup_opened');
+  popupElement.classList.remove('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
 
-    nameElement.textContent = nameInput.value;
-    descriptionElement.textContent = descriptionInput.value;
+  nameElement.textContent = nameInput.value;
+  descriptionElement.textContent = descriptionInput.value;
 
-    closePopup();
+  closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function createElement(caption, image) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  
+  const elementClone = elementTemplate.querySelector('.element').cloneNode(true);
+
+  elementClone.querySelector('.element__caption').textContent = caption;
+  elementClone.querySelector('.element__image').src = image;
+
+  elements.append(elementClone);
+}
+
+const elements = document.querySelector('.elements__list');
+
+// функция перебора массива
+initialCards.forEach(function (card) {
+  createElement(card.name, card.link);
+});
