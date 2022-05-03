392f89b5496bda2d2ea6112b284cd8e4cf8d3469
Julia Kosobokova <jilika12@yandex.ru>
2022-05-03 19:16:14 +0300

    fix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,29 @@
-const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
 
 const popupProfile = document.querySelector('.popup_profile');
-const closeButtonProfile = popupProfile.querySelector('.popup__close-button');
+const buttonCloseProfile = popupProfile.querySelector('.popup__close-button');
 
 const popupElement = document.querySelector('.popup_element');
-const closeButtonElement = popupElement.querySelector('.popup__close-button');
+const buttonCloseElement = popupElement.querySelector('.popup__close-button');
 
 const popupImage = document.querySelector('.popup_image');
-const closeButtonImage = popupImage.querySelector('.popup__close-button');
+const buttonCloseImage = popupImage.querySelector('.popup__close-button');
 
-const nameProfileInput = popupProfile.querySelector('.popup__input_type_name');
-const descriptionProfileInput = popupProfile.querySelector('.popup__input_type_description');
+const inputProfileName = popupProfile.querySelector('.popup__input_type_name');
+const inputProfileDescription = popupProfile.querySelector('.popup__input_type_description');
 const nameProfile = document.querySelector('.profile__name');
 const descriptionProfile = document.querySelector('.profile__description');
-const profileForm = popupProfile.querySelector('.popup__form');
+const formProfile = popupProfile.querySelector('.popup__form');
 
-const nameElementInput = popupElement.querySelector('.popup__input_type_name');
-const linkElementInput = popupElement.querySelector('.popup__input_type_link');
-const elementForm = popupElement.querySelector('.popup__form');
+const inputElementName = popupElement.querySelector('.popup__input_type_name');
+const inputElementLink = popupElement.querySelector('.popup__input_type_link');
+const formElement = popupElement.querySelector('.popup__form');
+
+const elementTemplate = document.querySelector('#element-template').content;
+
+const image = popupImage.querySelector('.popup__image');
+const text = popupImage.querySelector('.popup__text');
 
 // Открытие popup
 function openPopup(popup) {
@@ -27,29 +32,29 @@ function openPopup(popup) {
 
 // Popup окна редактирования профиля
 function openPopupProfile() {
+  inputProfileName.value = nameProfile.textContent;
+  inputProfileDescription.value = descriptionProfile.textContent;
   openPopup(popupProfile);
-  nameProfileInput.value = nameProfile.textContent;
-  descriptionProfileInput.value = descriptionProfile.textContent;
 }
 
 // Popup окна добавления карточки
 function openPopupElement() {
   openPopup(popupElement);
-  nameElementInput.value = '';
-  linkElementInput.value = '';
+  inputElementName.value = '';
+  inputElementLink.value = '';
 }
 
-editButton.addEventListener('click', () => openPopupProfile());
-addButton.addEventListener('click', () => openPopupElement());
+buttonEdit.addEventListener('click', () => openPopupProfile());
+buttonAdd.addEventListener('click', () => openPopupElement());
 
 //Закрытие popup
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-closeButtonProfile.addEventListener('click', () => closePopup(popupProfile));
-closeButtonElement.addEventListener('click', () => closePopup(popupElement));
-closeButtonImage.addEventListener('click', () => closePopup(popupImage));
+buttonCloseProfile.addEventListener('click', () => closePopup(popupProfile));
+buttonCloseElement.addEventListener('click', () => closePopup(popupElement));
+buttonCloseImage.addEventListener('click', () => closePopup(popupImage));
 
 // Обработчик «отправки» формы редактирования профиля, хотя пока
 // она никуда отправляться не будет
@@ -58,48 +63,20 @@ function handleProfileFormSubmit(evt) {
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
-  nameProfile.textContent = nameProfileInput.value;
-  descriptionProfile.textContent = descriptionProfileInput.value;
+  nameProfile.textContent = inputProfileName.value;
+  descriptionProfile.textContent = inputProfileDescription.value;
 
   closePopup(popupProfile);
 }
 
-profileForm.addEventListener('submit', handleProfileFormSubmit);
-
-//Карточки
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+formProfile.addEventListener('submit', handleProfileFormSubmit);
 
 // функция создания новой карточки
 function createElement(caption, image) {
-  const elementTemplate = document.querySelector('#element-template').content;
   const elementClone = elementTemplate.querySelector('.element').cloneNode(true);
 
-  const likeButton = elementClone.querySelector('.element__like');
-  const trashButton = elementClone.querySelector('.element__trash');
+  const buttonLike = elementClone.querySelector('.element__like');
+  const buttonTrash = elementClone.querySelector('.element__trash');
 
   const imageElement = elementClone.querySelector('.element__image');
   const captionElement = elementClone.querySelector('.element__caption');
@@ -108,11 +85,11 @@ function createElement(caption, image) {
   imageElement.src = image;
   imageElement.alt = caption;
 
-  likeButton.addEventListener('click', function (evt) {
+  buttonLike.addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_active');
   });
 
-  trashButton.addEventListener('click', function () {
+  buttonTrash.addEventListener('click', function () {
     elementClone.remove();
   });
 
@@ -120,21 +97,19 @@ function createElement(caption, image) {
     openPopupImage(evt.target.src, evt.target.alt);
   });
 
-  elements.prepend(elementClone);
+  return elementClone;
 }
 
 const elements = document.querySelector('.elements__list');
 
 // функция перебора массива
 initialCards.forEach(function (card) {
-  createElement(card.name, card.link);
+  const element = createElement(card.name, card.link);
+  elements.prepend(element);
 });
 
 // Функция открытия карточки
 function openPopupImage(link, caption) {
-  const image = popupImage.querySelector('.popup__image');
-  const text = popupImage.querySelector('.popup__text');
-
   openPopup(popupImage);
   image.src = link;
   image.alt = caption;
@@ -148,9 +123,10 @@ function handleElementFormSubmit(evt) {
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
-  createElement(nameElementInput.value, linkElementInput.value);
+  const element = createElement(inputElementName.value, inputElementLink.value);
+  elements.prepend(element);
 
   closePopup(popupElement);
 }
 
-elementForm.addEventListener('submit', handleElementFormSubmit);
+formElement.addEventListener('submit', handleElementFormSubmit);
