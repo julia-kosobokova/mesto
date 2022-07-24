a83dbdab75b63a3841aa0168ab5df3d3f462d67b
Julia Kosobokova <jilika12@yandex.ru>
2022-07-24 23:36:39 +0300

    faet: настроить сборку Вебпаком

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // подключите плагин 
+
+module.exports = {
+    entry: {main: './scripts/index.js'},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.[hash].js',
+           // publicPath: ''
+      },
+      mode: 'production',
+      devServer: {
+        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    
+        open: true // сайт будет открываться сам при запуске npm run dev
+      },
+      module: {
+        rules: [ // rules — это массив правил добавим в него объект правил для бабеля
+          {
+            // регулярное выражение, которое ищет все js файлы
+            test: /\.js$/,
+            // при обработке этих файлов нужно использовать babel-loader
+            use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                }
+            },
+            // исключает папку node_modules, файлы в ней обрабатывать не нужно
+            exclude: '/node_modules/'
+          },
+          {
+            // регулярное выражение, которое ищет все файлы с такими расширениями
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            // применять это правило только к CSS-файлам
+            test: /\.css$/,
+            // при обработке этих файлов нужно использовать
+            // MiniCssExtractPlugin.loader и css-loader
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            //'postcss-loader'
+          ]
+          },
+    ]
+     },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './index.html' // путь к файлу index.html
+        }),
+        new CleanWebpackPlugin(), // использовали плагин
+        new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+      ],
+      // loaders: [{
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     }
+      //   }
+      // }
+
+      // ]
+};
